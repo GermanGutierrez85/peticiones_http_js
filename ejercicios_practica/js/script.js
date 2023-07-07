10718b82d671164f0a0a087b89a8d9730bf75318
Germán Gutiérrez <128050377+GermanGutierrez85@users.noreply.github.com>
2023-07-07 20:44:44 -0300

    Add files via upload

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -8,3 +8,50 @@
 /* Enunciado
     --> Leer el README para ver el enunciado
 */
+
+const btnConsultar = document.getElementById('btnConsultar');
+const dominio = "https://rickandmortyapi.com";
+
+btnConsultar.onclick = async () => {
+    //Capturando el personaje que se quiere consultar
+    const personaje = document.querySelector('#personaje').value;
+    if(personaje == ""){
+        alert("Ingrese un personaje")
+    }
+    
+
+    //Consultando a la API
+    const url = `${dominio}/api/character/?name=${personaje}`;
+    console.log(url)
+
+    const resp = await fetch(url);
+    if(resp.ok){
+      const data = await resp.json()
+      const info = data.results[0]
+      console.table(info); 
+
+      let avatar = `<div id="characterCard">
+                       <img id="image" src=${info.image} alt="">
+                        <div id="info">
+                              <p id="name">${info.name}</p>
+                              <p class="species">${info.status} - ${info.species}</p>
+                                 <p class="status">Last know location:</p>
+                                 <li class="data">${info.location.name}</li>
+                                 <p class="status">First seen in:</p> 
+                                 <li class="data">${info.origin.name}</li>
+                          </div>
+                       </div>  
+                      `
+        const card = document.createElement("section")
+        card.innerHTML = avatar;
+        document.body.appendChild(card);
+
+     }else{
+        console.error('Response returner with errors:', info);
+        throw new Error('Error al obtener el personaje')
+     }
+
+    
+    
+ };
+ 
